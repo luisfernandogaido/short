57bbd3263359ef0fc815a07be6bc4076ca3da32c
Luís Fernando Gaido <luisfernandogaido@gmail.com>
2021-03-12 14:35:47 -0300

    flag parse depois

--- example/main.go
+++ example/main.go
@@ -15,12 +15,12 @@ func main() {
 		redisURI  *string
 		domain    *string
 	)
-	flag.Parse()
 	addr = flag.String("addr", ":4018", "porta local")
 	mongoURI = flag.String("mongo_uri", "mongodb://localhost:27017", "string de conexão mongoDB")
 	tokenRoot = flag.String("token_root", "MDKTQuSFiUleok6GA0MF", "token administrativo, crud usuários autorizados")
 	redisURI = flag.String("redis_uri", "redis://localhost:6379", "string de conexão Redis")
 	domain = flag.String("domain", "http://localhost:4018", "domínio que hospeda o serviço")
+	flag.Parse()
 	if err := api.Serve(*addr, *mongoURI, *tokenRoot, *redisURI, *domain); err != nil {
 		log.Fatal(err)
 	}
